030dd94d678780a3a1cb18f74922881d48c03083
theWebby <jameswebb15@hotmail.co.uk>
2020-09-08 19:21:38 +0100

    log status

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,4 @@
 sudo docker build --tag yms-server:1.0 .
 sudo docker stop $(sudo docker ps -a -q)
-sudo docker run -it -v /etc/letsencrypt/:/etc/letsencrypt/:ro -v ./logs:./logs -p 3000:3000 -p 443:443 --restart unless-stopped yms-server:1.0
+sudo docker run -it -v /etc/letsencrypt/:/etc/letsencrypt/:ro -v /home/ubuntu/YoutubeMySpotify-Server/server/logs:/usr/src/app/logs -p 3000:3000 -p 443:443 --restart unless-stopped yms-server:1.0
 
